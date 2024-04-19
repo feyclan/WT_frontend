dbdbca16fea1ff2a34cb72d00a58ad61eccb3962
Ilias <ilias.azagagh@icloud.com>
2024-04-19 10:30:28 +0200

    WIP: loan history

---
+++ src/dto/ReadBookCopyDto.ts
@@ -0,0 +1,6 @@
+export class ReadBookCopyDto {
+    id?: number;
+    isAvailable?: boolean;
+    state?: string;
+    bookId?: number;
+}
---
+++ src/dto/ReadLoanDto.ts
@@ -0,0 +1,9 @@
+export class LoanDto {
+    id?: number;
+    conditionStart?: string;
+    conditionEnd?: string;
+    startDate?: Date;
+    endDate?: Date;
+    bookCopyId?: number;
+    userId?: number;
+}
