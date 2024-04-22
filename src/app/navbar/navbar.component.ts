34fc01e74b0da12d4fd69174bbd9f93684d9d946
Rik131 <rik.13@live.nl>
2024-04-22 15:05:35 +0200

    add name and role to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,6 +13,7 @@ import { RouterModule } from '@angular/router';
 export class NavbarComponent {
 
   role: string | null = null;
+  name: string | null = null;
 
   constructor(private dataSharingService: DataSharingService) {
 
@@ -23,6 +24,7 @@ export class NavbarComponent {
       console.log('NAVBAR: User has changed');
 
       this.role = localStorage.getItem('WT_ROLE');
+      this.name = localStorage.getItem('WT_NAME');
     })
   }
 
