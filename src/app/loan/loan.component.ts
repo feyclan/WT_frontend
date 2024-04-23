c9106e530855f93005f4a7c6ff580a6ccc8f7c94
TheAaltje23 <a.van.noordenne@hotmail.com>
2024-04-23 16:41:35 +0200

    added boek innemen functionality + modal

--- src/app/loan/loan.component.ts
+++ src/app/loan/loan.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { LoanDto } from '../../dto/ReadLoanDto';
 import { ReadBookDto } from '../../dto/ReadBookDto';
 import { ReadUserDto } from '../../dto/ReadUserDto';
 import { UserService } from '../user.service';
 import { BookCopyService } from '../bookCopy.service';
 import { BookService } from '../book.service';
+import { LoanService } from '../loan.service';
 import { ReadBookCopyDto } from '../../dto/ReadBookCopyDto';
 import { CommonModule } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: '[app-loan]',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './loan.component.html',
   styleUrl: './loan.component.scss'
 })
@@ -23,13 +26,23 @@ export class LoanComponent {
   bookCopy: ReadBookCopyDto | null = null;
   book: ReadBookDto | null = null;
   user: ReadUserDto | null = null;
+  innemenForm: FormGroup;
+  modalTitle: string = 'Boek Innemen';
 
   // All services needed to access the proper end points
   constructor(
     private bookCopyService: BookCopyService,
     private userService: UserService,
-    private bookService: BookService
-  ) { }
+    private bookService: BookService,
+    private loanService: LoanService,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder
+  ) {
+    this.innemenForm = this.formBuilder.group({
+      conditionEnd: ['', Validators.required],
+      endDate: ['', Validators.required]
+    });
+  }
 
   // Get all the foreign keys necessary to construct all the loan information
   ngOnInit(): void {
@@ -57,5 +70,29 @@ export class LoanComponent {
     this.bookService.getBook(id).subscribe(resp => {
       this.book = resp.data;
     })
+  }
+
+  // Open modal for boek innemen
+  openConditionModal(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      if (result === 'save') {
+        // change endcondition
+        if (this.loan) {
+          this.loan.conditionEnd = this.innemenForm.value.conditionEnd;
+          this.loan.endDate = this.innemenForm.value.endDate;
+          this.loan.isActive = false;
+
+          this.loanService.updateLoan(this.loan).subscribe(response => {
+            if (response.success) {
+              console.log('response', response);
+            } else {
+              alert(response.errors);
+            }
+          });
+        }
+      }
+    }, (reason) => {
+      console.log('Boek inneming geanulleerd:', reason);
+    });
   }
 }
