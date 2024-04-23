b8b39db0e69a539ffbe558a00dc6f5a2756693ce
Ilias <ilias.azagagh@icloud.com>
2024-04-23 16:08:01 +0200

    Skeleton for getting loans and reservations of specific user

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -3,28 +3,51 @@ import { Component, Input } from '@angular/core';
 import { ReadUserDto } from '../../dto/ReadUserDto';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
+import { LoanService } from '../loan.service';
+import { ReservationService } from '../reservation.service';
+import { LoanComponent } from '../loan/loan.component';
+import { UserReservationComponent } from '../user-reservation/user-reservation.component';
+import { LoanDto } from '../../dto/ReadLoanDto';
+import { ReservationDto } from '../../dto/ReadReservationDto';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoanComponent, UserReservationComponent],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
 export class UserDetailsComponent {
   @Input() user: ReadUserDto | null = null;
-  id: number | 0 = 0;
+  userId: number | 0 = 0;
+  loans = new Array<LoanDto>();
+  userReservations = new Array<ReservationDto>();
 
   // Get the user by reading the param from the url
-  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
-    this.id = this.activatedRoute.snapshot.params['id'];
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private loanService: LoanService,
+    private reservationService: ReservationService
+  ) {
+    this.userId = this.activatedRoute.snapshot.params['id'];
 
-    this.getUser();
+    this.getUser(this.userId);
+    this.getReservations();
+    this.getLoans(this.userId);
   }
 
-  getUser() {
-    this.userService.getUser(this.id).subscribe(resp => {
+  getUser(id: number) {
+    this.userService.getUser(id).subscribe(resp => {
       this.user = resp.data;
     })
+  }
+
+  // Create endpoint to get user loans by id
+  getLoans(id: number) {
+  }
+
+  // Create endpoint to get user reservations by id
+  getReservations() {
   }
 }
