7124f052f63472abd53cb0c407e83d40d5b305cf
Ilias <ilias.azagagh@icloud.com>
2024-04-23 15:52:59 +0200

    Show user name on user detail page

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ReadUserDto } from '../../dto/ReadUserDto';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
 export class UserDetailsComponent {
+  @Input() user: ReadUserDto | null = null;
+  id: number | 0 = 0;
 
+  // Get the user by reading the param from the url
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
+    this.id = this.activatedRoute.snapshot.params['id'];
+
+    this.getUser();
+  }
+
+  getUser() {
+    this.userService.getUser(this.id).subscribe(resp => {
+      this.user = resp.data;
+    })
+  }
 }
