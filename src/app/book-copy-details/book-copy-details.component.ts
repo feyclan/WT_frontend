adcdd80dadf392b19922a65e6517aacafdc061d2
Rik131 <rik.13@live.nl>
2024-04-25 11:19:17 +0200

    use routerLink

--- src/app/book-copy-details/book-copy-details.component.ts
+++ src/app/book-copy-details/book-copy-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ReadBookCopyDto } from '../../dto/ReadBookCopyDto';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: '[app-book-copy-details]',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './book-copy-details.component.html',
   styleUrl: './book-copy-details.component.scss'
 })
