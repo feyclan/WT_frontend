c2ddc4b3bac1687d0ad082a0f2af7f356064454f
Ilias <ilias.azagagh@icloud.com>
2024-04-25 07:41:01 +0200

    Removed duplicate

--- src/app/loan-list/loan-list.component.ts
+++ src/app/loan-list/loan-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoanDto } from '../../dto/ReadLoanDto';
 import { LoanService } from '../loan.service';
@@ -8,6 +8,9 @@ import { UserReservationComponent } from '../user-reservation/user-reservation.c
 import { ReservationService } from '../reservation.service';
 import { DataSharingService } from '../data-sharing.service';
 import { ReservationComponent } from '../reservation/reservation.component';
+import { ReadUserDto } from '../../dto/ReadUserDto';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-loan-list',
@@ -17,36 +20,67 @@ import { ReservationComponent } from '../reservation/reservation.component';
   styleUrl: './loan-list.component.scss'
 })
 export class LoanListComponent {
+  @Input() user: ReadUserDto | null = null;
+  userId: number | 0 = 0;
   role: string | null = null;
-  name: string | null = null;
   loans = new Array<LoanDto>();
   reservations = new Array<ReservationDto>();
 
   constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
     private loanService: LoanService,
     private reservationService: ReservationService,
     private dataSharingService: DataSharingService
-  ) { }
+  ) {
+    // Get user id from the url
+    this.userId = this.activatedRoute.snapshot.params['id'];
+
+    console.log("userid", this.userId);
+
+    if (this.userId) {
+      this.getUser(this.userId);
+      this.getReservationsById(this.userId);
+      this.getLoansById(this.userId);
+    } else {
+      this.getUserReservations();
+      this.getUserLoans();
+    }
+  }
 
   ngOnInit(): void {
-    this.loadUserLoans();
-    this.loadUserReservations();
-
-    // Get the role and name of the current user
+    // Get the role of the current user
     this.dataSharingService.userChangeObservable.subscribe(() => {
       this.role = localStorage.getItem('WT_ROLE');
-      this.name = localStorage.getItem('WT_NAME');
     })
   }
 
-  loadUserLoans() {
+  getUserLoans() {
     this.loanService.getUserLoans().subscribe(resp => {
       this.loans = resp.data;
     })
   }
 
-  loadUserReservations() {
+  getUserReservations() {
     this.reservationService.getUserReservations().subscribe(resp => {
+      this.reservations = resp.data;
+    })
+  }
+
+  getUser(id: number) {
+    this.userService.getUser(id).subscribe(resp => {
+      this.user = resp.data;
+    })
+  }
+
+  getLoansById(id: number) {
+    this.loanService.getLoansByUserId(id).subscribe(resp => {
+      this.loans = resp.data;
+    })
+  }
+
+  getReservationsById(id: number) {
+    this.reservationService.getReservationsByUserId(id).subscribe(resp => {
       this.reservations = resp.data;
     })
   }
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,16 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { ReadUserDto } from '../../dto/ReadUserDto';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { UserService } from '../user.service';
-import { LoanService } from '../loan.service';
-import { ReservationService } from '../reservation.service';
 import { LoanComponent } from '../loan/loan.component';
-import { LoanDto } from '../../dto/ReadLoanDto';
-import { ReservationDto } from '../../dto/ReadReservationDto';
 import { ReservationComponent } from '../reservation/reservation.component';
 import { LoanListComponent } from '../loan-list/loan-list.component';
-import { DataSharingService } from '../data-sharing.service';
 
 @Component({
   selector: 'app-user-details',
@@ -20,49 +13,7 @@ import { DataSharingService } from '../data-sharing.service';
   styleUrl: './user-details.component.scss'
 })
 export class UserDetailsComponent {
-  @Input() user: ReadUserDto | null = null;
-  userId: number | 0 = 0;
-  role: string | null = null;
-  loans = new Array<LoanDto>();
-  reservations = new Array<ReservationDto>();
 
   // Get the user by reading the param from the url
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private userService: UserService,
-    private loanService: LoanService,
-    private reservationService: ReservationService,
-    private dataSharingService: DataSharingService,
-  ) {
-    this.userId = this.activatedRoute.snapshot.params['id'];
-
-    this.getUser(this.userId);
-    this.getReservations(this.userId);
-    this.getLoans(this.userId);
-
-    // Get the role of the current user
-    this.dataSharingService.userChangeObservable.subscribe(() => {
-      this.role = localStorage.getItem('WT_ROLE');
-    })
-  }
-
-  getUser(id: number) {
-    this.userService.getUser(id).subscribe(resp => {
-      this.user = resp.data;
-    })
-  }
-
-  // Create endpoint to get user loans by id
-  getLoans(id: number) {
-    this.loanService.getLoansByUserId(id).subscribe(resp => {
-      this.loans = resp.data;
-    })
-  }
-
-  // Create endpoint to get user reservations by id
-  getReservations(id: number) {
-    this.reservationService.getReservationsByUserId(id).subscribe(resp => {
-      this.reservations = resp.data;
-    })
-  }
+  constructor() {}
 }
